4a054eb2de5c20fe3b2a4df6501811712b337ddf
DevSecOps <kevinschier@alumni.fh-aachen.de>
2024-12-06 14:00:23 +0100

    fix sql injection

--- PHPAppSnippets/low.php
+++ PHPAppSnippets/low.php
@@ -7,10 +7,14 @@ if( isset( $_GET[ 'Login' ] ) ) {
 	// Get password
 	$pass = $_GET[ 'password' ];
 	$pass = md5( $pass );
+	
+	$query = "SELECT * FROM users WHERE user = :user AND password = :pass";
 
-	// Check the database
-	$query  = "SELECT * FROM `users` WHERE user = '$user' AND password = '$pass';";
-	$result = mysqli_query($GLOBALS["___mysqli_ston"],  $query ) or die( '<pre>' . ((is_object($GLOBALS["___mysqli_ston"])) ? mysqli_error($GLOBALS["___mysqli_ston"]) : (($___mysqli_res = mysqli_connect_error()) ? $___mysqli_res : false)) . '</pre>' );
+        $stmt = $GLOBALS["___mysqli_ston"]->prepare($query);
+        $stmt->bind_param(":user", $user);
+        $stmt->bind_param(":pass", $pass);
+        $stmt->execute();
+        $result = $stmt->store_result();
 
 	if( $result && mysqli_num_rows( $result ) == 1 ) {
 		// Get users details
